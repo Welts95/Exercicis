47025fe862d06dc54c11aad2ac3ca7dcbbb3d844
administrador <gerard.hueltes@gmail.com>
2022-03-15 16:36:51 +0100

    vinté commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
 import llista from "./pages/Llista";
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -15,6 +15,6 @@ export default function App() {
           ))}
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
